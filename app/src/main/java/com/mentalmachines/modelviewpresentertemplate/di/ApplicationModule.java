be1e6609570b52a83e35b62e450e2e4e79e7c14c
kimj <jinn.kim@gmail.com>
2017-01-31 17:34:23 -0500

    adding dependancy injection

---
+++ app/src/main/java/com/mentalmachines/modelviewpresentertemplate/di/ApplicationModule.java
@@ -0,0 +1,8 @@
+package com.mentalmachines.modelviewpresentertemplate.di;
+
+/**
+ * Created by jkim11 on 1/31/17.
+ */
+
+public class ApplicationModule {
+}
---
+++ app/src/main/java/com/mentalmachines/modelviewpresentertemplate/views/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.mentalmachines.modelviewpresentertemplate.views;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jkim11 on 1/31/17.
+ */
+
+public class BaseFragment extends Fragment{
+}
